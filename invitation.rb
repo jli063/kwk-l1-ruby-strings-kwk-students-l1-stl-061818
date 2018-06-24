017fadd1d5a5150dd09cd5abf1a339954e2755a4
jli063 <jli063@rsdmo.org>
2018-06-24 22:17:43 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -6,10 +6,13 @@
 
 
 def invitation
-puts "Dear #{guest_name}," 
-puts "You are cordially invited to the Best #{party_name} 
-Party Ever on #{date} at #{time}.
-Please RSVP no later than October 30.
+  puts "Hi, you've been invited to a party! What is your name?"
+  puts "Dear #{guest_name}," 
+  puts "You are cordially invited to the Best #{party_name} 
+  Party Ever on #{date} at #{time}.
+  Please RSVP no later than October 30.
 
-Sincerely,
+  Sincerely,
 #{host_name}"
+end
+invitation
