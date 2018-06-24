693181a31c8f6510996ae09c3935abbe43188860
jli063 <jli063@rsdmo.org>
2018-06-24 22:22:28 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -9,11 +9,19 @@ def invitation
   puts "Hi, you've been invited to a party! What is your name?"
   guest_name=gets.chomp
   puts "Dear #{guest_name}," 
-  puts "You are cordially invited to the Best #{party_name} 
-  Party Ever on #{date} at #{time}.
-  Please RSVP no later than October 30.
-
-  Sincerely,
-#{host_name}"
+  puts "What party have you been invited to?"
+  party_name=gets.chomp
+  puts "You are cordially invited to the Best #{party_name} Party Ever"
+  puts "What date is the party?"
+  date=gets.chomp
+  puts "on #{date}"
+  puts "What time is it?"
+  time=gets.chomp
+  puts "at #{time}
+  Please RSVP no later than October 30."
+  puts "What is the host name?"
+  host_name=gets.chomp
+  puts "Sincerely,
+  #{host_name}"
 end
 invitation
